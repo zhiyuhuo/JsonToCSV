8917145d30a47906138b42806ca9d2b87b87e7a4
Zhiyu Huo <zhdy7@mail.missouri.edu>
2016-02-18 16:33:18 -0600

    add newtonsoft

--- JsonToCSV/JsonReader.cs
+++ JsonToCSV/JsonReader.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JsonToCSV
 {
     class JsonReader
     {
+        Newtonsoft.Json.j
+        String m_jsonFileDir = "";
+        JObject o1 = JObject.Parse(File.ReadAllText(@"c:\videogames.json"));
 
     }
 }
